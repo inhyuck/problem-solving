0df3d19af9010928a4f2c747a15dde1e08ff931c
INHYCUK <dlsgur1447@gmail.com>
2018-07-06 20:40:55 +0900

    초기세팅 + 2557문제풀이

---
+++ skhucode/src/q2557/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Date: 2018. 07. 06.
+ * URL: https://www.acmicpc.net/problem/2557
+ * Title: Hello World
+ * Problem: Hello World!를 출력하시오.
+ */
+package q2557;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
