292c76e2521a032a188588f7951d9a4e3aeabc42
INHYCUK <dlsgur1447@gmail.com>
2018-07-06 20:51:35 +0900

    Q1000 A+B

---
+++ skhucode/src/q1000/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Date: 2018. 07. 06.
+ * URL: https://www.acmicpc.net/problem/1000
+ * Title: A+B
+ * Problem: 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ */
+package q1000;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
