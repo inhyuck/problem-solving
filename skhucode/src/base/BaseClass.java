977b834eb6d49f14fc8cc6cde686e53c7e4e5426
INHYCUK <dlsgur1447@gmail.com>
2018-07-18 16:52:44 +0900

    base class 생성

---
+++ skhucode/src/base/BaseClass.java
@@ -0,0 +1,29 @@
+/**
+ * Date: 2018. 07. 18.
+ * URL: https://www.acmicpc.net/problem/?
+ * Title:
+ * Problem:
+ */
+package base;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Scanner
+ */
+public class BaseClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
+
+/**
+ * BufferedReader
+ */
+//public class BaseClass {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//    }
+//}
