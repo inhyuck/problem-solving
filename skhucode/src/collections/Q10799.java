b25a72fab9a60a58b53e204e6066ac2633b02c87
INHYCUK <dlsgur1447@gmail.com>
2018-07-29 23:42:08 +0900

    README.md 수정 및 Q10799, Q9012추가

---
+++ skhucode/src/collections/Q10799.java
@@ -0,0 +1,32 @@
+/**
+ * Date: 2018. 07. 28.
+ * URL: https://www.acmicpc.net/problem/10799
+ * Title: 쇠막대기
+ * Problem: 링크 참고
+ */
+package collections;
+
+import java.util.*;
+
+public class Q10799 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                if (i == stack.peek() + 1) { //레이저인 경우
+                    count += stack.size() - 1;
+                } else { //쇠막대기 하나가 끝남
+                    count++;
+                }
+                stack.pop();
+            }
+        }
+        System.out.println(count);
+    }
+}
